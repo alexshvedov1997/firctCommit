66bf0dc37ef90a56f6da7c3f866946014bbdead4
Alex Shvedov <alexshvedov1997@gmail.com>
2019-09-29 12:38:16 +0300

    rt

---
+++ QuickSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = { 3, 8, 9, 3, 4, 0, 1, 23 };
+            QuickSort(mass, 0, 7);
+            foreach (int p in mass) Console.WriteLine(p);
+
+        }
+        static void QuickSort(int[] mass, int first, int last) {
+            int f = first, l = last;
+            int count;
+            int mid = mass[(f + l) / 2];
+            do
+            {
+                while (mass[f] > mid) f++;
+                while (mass[l] < mid) l--;
+                if (f <= l)
+                {
+                    count = mass[f];
+                    mass[f] = mass[l];
+                    mass[l] = count;
+                    f++;
+                    l--;
+                }
+
+            } while (f < l);
+            if (first < l) QuickSort(mass, first, l);
+            if (f < last) QuickSort(mass, f, last);
+
+        }
+    }
+}
